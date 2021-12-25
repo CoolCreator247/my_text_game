dcb7336110cb99224c61425376a401f4a374ef7b
Faris Allafi <96661970+CoolCreator247@users.noreply.github.com>
2021-12-25 14:04:58 -0800

    Update part 1.py

--- part 1.py
+++ part 1.py
@@ -1,1 +1,27 @@
 print("start")
+while True:
+    print("you can:")
+    print("1.get food")
+    print("2.get water")
+    print("3.get bloks")
+    print("4.make a home")
+    input(">")
+    answer = "0"
+    answer = input()
+
+    if answer == "1":
+        yesNo = ""
+        print("you got food eat it yes/no")
+        yesNo = input()
+        if yesNo == "yes":
+            print("you lived")
+        elif yesNo == "no":
+            print("You died from hunger")
+
+    
+    if answer == "2":
+        print("got water")
+        print("yes/no/filter")
+        if answer == "yes"
+            print("it was posind")
+        if answer ==    
