963f0f99649e56b89121fbb151057e3a4614e242
Faris Allafi <farisqaisallafi@gmail.com>
2022-06-28 01:58:56 +0000

    fixes

--- my text game.py
+++ my text game.py
@@ -4,14 +4,15 @@ print("2.get water")
 print("3.work to get money")
 print("4.get blocks")
 print("5.make a home")
+print("6.quit")
 blocks = ""
 money = 0
 while True:
-    answer = "0"
+    answer = " "
     answer = input(">")
 
     if answer == "1":
-        yesNo = ""
+        yesNo = "0"
         print("you got food eat it yes/no")
         yesNo = input()
         if yesNo == "yes":
@@ -20,7 +21,7 @@ while True:
             print("You did not survive from hunger Go back and try something else")
 
     
-    if answer == "2":
+    elif answer == "2":
         print("got water")
         print("yes/no/filter")
         yesNo = input()
@@ -31,25 +32,29 @@ while True:
         if yesNo == "filter":
             print("you lived") 
 
-    if answer == "3":
+    elif answer == "3":
         print("you got 25 dollers come back to get more")       
         money = money + 25
 
-    if answer == "4":
+    elif answer == "4":
         print("you have " +str(money) +" dollers")
         print("a block is 5 dollers")
+        print("how many do you want")
         blocks = input()
         print(blocks)
         money = money - int(blocks) * 5 
         print(f"you have $",money)
 
 
-    if answer =="5":
-        print(blocks)
+    elif answer =="5":
         if blocks == "":
             print("Sorry! you need more blocks!")
-    elif int(blocks) >= 10 :
-            print("you need more blocks")
-    elif int(blocks) < 10:
+        elif int(blocks) >= 10 :
+                print("you need more blocks")
+        elif int(blocks) < 10:
             print("you got a house,the end")
-            break
+
+        elif answer == "6":
+            print("good bye")
+            exit()       
+                
